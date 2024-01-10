60d4e0f8b29b33151e2ca2a6991ca8432561964b
tracer-ics2019 <tracer@njuics.org>
2024-01-11 00:58:05 +0800

    >  compile U202015543 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:58:05 up  3:00,  1 user,  load average: 0.12, 0.07, 0.02 b813107df224afd296de0bc237618d58426550c6

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -11,6 +11,8 @@ void isa_reg_display() {
 	//pa1: display registers like GDB
 	for(int i=0; i<32; i++)
 		printf("%s\t0x%08x\t%d\n", regsl[i], reg_l(i), reg_l(i));
+	printf("pc\t0x%08x\t%d\n", cpu.pc, cpu.pc);
+	
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
