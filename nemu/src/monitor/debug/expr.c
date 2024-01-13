656d9d2e5a79364e9d7c5b5acac611cac68d7411
tracer-ics2019 <tracer@njuics.org>
2024-01-14 00:08:20 +0800

    >  compile U202015543 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:08:20 up  7:24,  1 user,  load average: 0.05, 0.07, 0.08 725fb060cb359a2061fff5f518fc4116e9cdcaf1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -285,7 +285,7 @@ uint32_t expr(char *e, bool *success) {
   {
     int formerTokenType = (i==0) ? TK_NOTYPE : tokens[i-1].type;
     //e.g. an expression like ( *p) is BNF, and 4 + *p is not BNF, it should be 4 + (*p)
-    if(tokens[i].type == '*' && (formerTokenType == 1 || formerTokenType == TK_NOTYPE || formerTokenType == TK_LEFT_PARENTHESIS))	//case == 1 corresponds above code "i==0"
+    if((tokens[i].type == '*') && (formerTokenType == TK_NOTYPE || formerTokenType == TK_LEFT_PARENTHESIS))
     {
       printf("TK_DEREF 2");
       tokens[i].type = TK_DEREF;
